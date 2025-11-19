0db828af18117129824c191f2eb89c75170ba03b
Asalsmohammed <asala21mohammed7@gmail.com>
2025-11-19 21:09:55 +0300

    Arry_part one

---
+++ src/Arrays/Main.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner a =new Scanner(System.in);
+        Arrayss arr =new Arrayss();
+        //int []numbers=new int[5];
+        int []numbers={1,2,3,4,5};
+
+      //arr.traversal(numbers);
+      arr.traversal2(numbers);
+      //numbers[0]=8;
+       // System.out.println(Arrays.toString(numbers));
+        System.out.println("please enter the"+numbers.length+" element");
+        numbers[0]= a.nextInt();
+
+
+
+
+
+
+    }
+    }
